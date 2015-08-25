02e286413a5e2d55ddceacda53c75c63d3a10a73
Tiff <kim.tiffany.min@gmail.com>
2015-08-25 15:08:20 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FpQuizTk::Application.config.secret_key_base = 'd3a82dac4638463f058a5c3aaf85b1a86757f43e1d55c13af8acbe5ae032f51c8cec342f5f943f886138759158dec20cc689c6b1b4652dc90fa9972faab69e7c'
